00ba3f61ab79716d52673ea975d771d64f5d314d
Catherine Dean <catherinemdean15@gmail.com>
2020-11-11 10:22:42 -0700

    add total_inventory method to Market

--- lib/market.rb
+++ lib/market.rb
@@ -23,5 +23,29 @@ class Market
     end
   end
 
+  def all_items
+    @vendors.flat_map do |vendor|
+      vendor.inventory.keys
+    end.uniq
+  end
+
+  def total_inventory
+    inventory = Hash.new({})
+    all_items.each do |item|
+      @vendors.each do |vendor|
+        (inventory[item][:quantity] += vendor.check_stock(item) if inventory[item][:quantity]) ||
+          (inventory[item][:quantity] = vendor.check_stock(item))
+        (inventory[item][:vendors] << vendor if inventory[item][:vendors]) ||
+          (inventory[item][:vendors] = [vendor])
+      end
+    complete_inventory = Hash.new()
+    vendors.each do |vendor|
+      vendor.inventory.keys.each do |item|
+        complete_inventory[item] = inventory[item]
+      end
+    end
+    complete_inventory
+  end
+
 
 end

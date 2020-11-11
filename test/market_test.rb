db470ef1006f407150d507a8c0966a88ec30c6e0
Catherine Dean <catherinemdean15@gmail.com>
2020-11-11 09:36:34 -0700

    add test_vendors_that_sell test to Market

--- test/market_test.rb
+++ test/market_test.rb
@@ -30,12 +30,6 @@ class MarketTest < Minitest::Test
   end
 
   def test_add_vendor
-    @vendor1.stock(@item1, 35)
-    @vendor1.stock(@item2, 7)
-    @vendor2.stock(@item4, 50)
-    @vendor2.stock(@item3, 25)
-    @vendor3.stock(@item1, 65)
-
     @market.add_vendor(@vendor1)
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
@@ -50,6 +44,20 @@ class MarketTest < Minitest::Test
 
     assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"],
       @market.vendor_names
+  end
+
+  def test_vendors_that_sell
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
   end
 
 end

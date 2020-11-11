bfc528f97a1d6baa0123985a6cd36518c2eea238
Catherine Dean <catherinemdean15@gmail.com>
2020-11-11 09:10:35 -0700

    add Vendor instance variables name and inventory

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,9 +1,23 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/vendor'
+require './lib/item'
 
 class VendorTest < Minitest::Test
 
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_calls_name
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+  end
+
+  def test_it_has_inventory
+    assert_equal ({}), @vendor.inventory
+  end
 
 
-end 
+end

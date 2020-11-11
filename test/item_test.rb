d31f0f3009501e9be6fbd205fc194eeb9ee1379a
Catherine Dean <catherinemdean15@gmail.com>
2020-11-11 09:07:38 -0700

    add name and price to Item

--- test/item_test.rb
+++ test/item_test.rb
@@ -5,11 +5,19 @@ require './lib/item'
 class ItemTest < Minitest::Test
 
   def setup
-    @item = Item.new
+    @item = Item.new({name: 'Peach', price: "$0.75"})
   end
 
   def test_it_exists
     assert_instance_of Item, @item
+  end
+
+  def test_it_calls_name
+    assert_equal "Peach", @item.name
+  end
+
+  def test_it_calls_price
+    assert_equal "$0.75", @item.price
   end
 
 end

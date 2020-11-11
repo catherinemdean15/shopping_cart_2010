61d54ade303d5e9bc7b1445289e6baca249557b8
Catherine Dean <catherinemdean15@gmail.com>
2020-11-11 09:34:31 -0700

    add test_vendor_names to Market test

--- test/market_test.rb
+++ test/market_test.rb
@@ -43,4 +43,13 @@ class MarketTest < Minitest::Test
     assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
   end
 
+  def test_vendor_names
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"],
+      @market.vendor_names
+  end
+
 end
